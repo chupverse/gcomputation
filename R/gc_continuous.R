4aabebe6764bc1228bc822c5c4d994505fdd832a
Matthieutrtr <matthieutrotreau@gmail.com>
2025-10-07 17:20:56 +0200

    add gc_continuous + print method associated

---
+++ R/gc_continuous.R
@@ -0,0 +1,15 @@
+gc_continuous <- function(formula, data, group, effect="ATE", model, param.tune=NULL, cv=10, boot.type="bcv",
+                      boot.number=500, boot.tune=FALSE, boot.mi = FALSE, progress=TRUE, seed=NULL, m=5, ...) {
+  
+  if (!is.logical(boot.mi)) {stop("The argument \"boot.mi\" needs to be a logical TRUE or FALSE")}
+  if (boot.mi == FALSE) {
+    return(.gc_continuous(formula=formula, data=data, group=group, effect=effect, model=model, param.tune=param.tune, cv=cv,
+                      boot.type=boot.type,boot.number=boot.number, boot.tune=boot.tune, progress=progress ,seed=seed))
+  }
+  if (boot.mi == TRUE) {
+    stop("multiple imputations method not yet implemented for gc_continuous")
+    # return(.miboot_gc_binary(formula=formula, data=data, group=group, effect=effect, model=model, param.tune=param.tune, cv=cv,
+    #                          boot.type=boot.type,boot.number=boot.number, boot.tune=boot.tune, progress=progress, seed=seed,
+    #                          m=m, ...))
+  }
+}
